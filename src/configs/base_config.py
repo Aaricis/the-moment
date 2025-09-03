ee0c5d336fc9bf7b4390f281096257ac2944d27e
Aaricis <947165603@qq.com>
2025-09-03 19:25:05 +0800

    feat: define constants

--- src/configs/base_config.py
+++ src/configs/base_config.py
@@ -1,5 +1,11 @@
 import os
+
+import torch
+
 from rag_config import base_dir
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+is_half = True if torch.cuda.is_available() else False
 
 # Deepseek-R1 path
 model_path = os.path.join(base_dir, 'model')
